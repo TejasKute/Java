a0c8d4a8578b596aadd4541d43758a5ebc8a229d
TEJAS S KUTE <87482740+TejasKute@users.noreply.github.com>
2022-06-23 15:56:23 +0530

    Add files via upload

---
+++ Lab Session/Assignment 23-06-2022/SnakeCase.java
@@ -0,0 +1,35 @@
+package corejava;
+
+public class SnakeCase 
+{
+	public String camelToSnake(String str)
+	 {
+		//Empty String
+String stt="";
+//Append First Character To Lower Case
+stt+=Character.toLowerCase(str.charAt(0));
+
+for(int i=1;i<str.length();i++)
+{
+ char ch=str.charAt(i);
+ //Checking The Characters Unicode Identifier or Not
+if (Character.isUnicodeIdentifierPart(ch))
+{
+stt=stt+'_';
+ stt=stt+Character.toLowerCase(ch);
+ }
+else
+{
+stt=stt+ch;
+ }  
+}
+return stt;
+ }
+
+public static void main(String[] args) 
+{
+	//Creating a Object of SnakeCase
+SnakeCase sc=new SnakeCase();
+System.out.println(sc.camelToSnake("TejasKute"));
+ }
+}
